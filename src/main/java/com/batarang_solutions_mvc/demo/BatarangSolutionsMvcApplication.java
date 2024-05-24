227e99c822b0faa062fba00b68dcf2474676cb0c
zotel1 <zotelsigel@gmail.com>
2024-05-23 23:03:52 -0300

    Arrancando el nuevo proyecto

---
+++ src/main/java/com/batarang_solutions_mvc/demo/BatarangSolutionsMvcApplication.java
@@ -0,0 +1,13 @@
+package com.batarang_solutions_mvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatarangSolutionsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatarangSolutionsMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batarang_solutions_mvc/demo/BatarangSolutionsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batarang_solutions_mvc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatarangSolutionsMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
